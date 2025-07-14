8b24a1a5a7d5b4d24d2500a825cf36d1b5a045f0
Milly <145469893+diamilisoianepereira@users.noreply.github.com>
2025-07-14 10:33:32 -0300

    Update admin.py

--- sistema_piscina/admin.py
+++ sistema_piscina/admin.py
@@ -1,1 +1,14 @@
 from django.contrib import admin
+from .models import Usuario, Piscina, Monitoramento
+
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('email', 'nome_completo', 'tipo')
+
+@admin.register(Piscina)
+class PiscinaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'data_criacao')
+
+@admin.register(Monitoramento)
+class MonitoramentoAdmin(admin.ModelAdmin):
+    list_display = ('piscina', 'ph', 'temperatura', 'data_medicao')
