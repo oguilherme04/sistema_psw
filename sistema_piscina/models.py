dd8468937a11b4114979680b54a15b2650822f36
Milly <145469893+diamilisoianepereira@users.noreply.github.com>
2025-07-14 10:35:29 -0300

    Update models.py

--- sistema_piscina/models.py
+++ sistema_piscina/models.py
@@ -26,3 +26,15 @@ class Piscina(models.Model):
     def __str__(self):
         return self.nome
 
+class Monitoramento(models.Model):
+    piscina = models.ForeignKey(Piscina, on_delete=models.CASCADE)
+    ph = models.DecimalField(max_digits=4, decimal_places=2)
+    temperatura = models.DecimalField(max_digits=5, decimal_places=2)
+    data_medicao = models.DateTimeField(auto_now_add=True)
+    usuario = models.ForeignKey(Usuario, on_delete=models.SET_NULL, null=True)
+
+
+    def __str__(self):
+        return f"{self.piscina.nome} - {self.data_medicao}"
+
+
